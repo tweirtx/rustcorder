57a1dec553f27000f618aa1fc836f755bdf75886
Travis Weir <tweirtx@gmail.com>
2020-05-23 04:05:56 -0500

    Start of receiving

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use serenity::{
 use std::fs;
 use serenity::model::id::ChannelId;
 use std::sync::Arc;
+use serenity::voice::AudioReceiver;
 
 struct VoiceManager;
 
@@ -14,6 +15,55 @@ impl TypeMapKey for VoiceManager {
 }
 
 struct Handler;
+
+struct Receiver;
+
+impl Receiver {
+    pub fn new() -> Self {
+        // You can manage state here, such as a buffer of audio packet bytes so
+        // you can later store them in intervals.
+        Self { }
+    }
+}
+
+impl AudioReceiver for Receiver {
+    fn speaking_update(&mut self, _ssrc: u32, _user_id: u64, _speaking: bool) {
+        // You can implement logic here so that you can differentiate users'
+        // SSRCs and map the SSRC to the User ID and maintain a state in
+        // `Receiver`. Using this map, you can map the `ssrc` in `voice_packet`
+        // to the user ID and handle their audio packets separately.
+    }
+
+    fn voice_packet(
+        &mut self,
+        ssrc: u32,
+        sequence: u16,
+        _timestamp: u32,
+        _stereo: bool,
+        data: &[i16],
+        compressed_size: usize,
+    ) {
+        println!("Audio packet's first 5 bytes: {:?}", data.get(..5));
+        println!(
+            "Audio packet sequence {:05} has {:04} bytes (decompressed from {}), SSRC {}",
+            sequence,
+            data.len(),
+            compressed_size,
+            ssrc,
+        );
+    }
+    fn client_connect(&mut self, _ssrc: u32, _user_id: u64) {
+        // You can implement your own logic here to handle a user who has joined the
+        // voice channel e.g., allocate structures, map their SSRC to User ID.
+    }
+
+    fn client_disconnect(&mut self, _user_id: u64) {
+        // You can implement your own logic here to handle a user who has left the
+        // voice channel e.g., finalise processing of statistics etc.
+        // You will typically need to map the User ID to their SSRC; observed when
+        // speaking or connecting.
+    }
+}
 
 impl EventHandler for Handler {
     fn message(&self, ctx: Context, message: Message) {
@@ -33,7 +83,9 @@ impl EventHandler for Handler {
                     let guild = message.guild_id;
                     match guild {
                         Some(i) => {
-                            manager.join(i, ChannelId(x));
+                            if let Some(handler) = manager.join(i, ChannelId(x)) {
+                                handler.listen(Some(Box::new(Receiver::new())));
+                            }
                         },
                         None => {
                             message.channel_id.say(&ctx.http, "Hey, wait a minute. This is a DM!");

0ff765ab7ef7b9f2cb7ada84f318db6e4c552622
Travis Weir <tweirtx@gmail.com>
2020-06-12 13:43:35 -0500

    Packets are coming through!

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,11 @@ use std::{sync::Arc, fs};
 use serenity::{
     async_trait,
     client::{bridge::voice::ClientVoiceManager, Client, Context, EventHandler},
-    framework::{
-        standard::{
-            macros::{group},
-        },
-    },
     model::{channel::Message, gateway::Ready, id::ChannelId},
     prelude::*,
     voice::AudioReceiver,
-    Result as SerenityResult,
 };
+use tokio::sync::Mutex;
 
 struct VoiceManager;
 
@@ -74,11 +69,7 @@ impl AudioReceiver for Receiver {
 }
 
 impl VoiceManager {
-    fn new() -> VoiceManager {}
 }
-
-#[group]
-struct General;
 
 #[tokio::main]
 async fn main() {
@@ -96,86 +87,15 @@ async fn main() {
         let mut data = client.data.write().await;
         data.insert::<VoiceManager>(Arc::clone(&client.voice_manager));
     }
+    client.start().await.expect("Error starting client");
 }
 
-/*#[command]
-async fn join(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
-    let connect_to = match args.single::<u64>() {
-        Ok(id) => ChannelId(id),
-        Err(_) => {
-            check_msg(msg.reply(ctx, "Requires a valid voice channel ID be given").await);
-
-            return Ok(());
-        },
-    };
-
-    let guild_id = match ctx.cache.guild_channel_field(msg.channel_id, |channel| channel.guild_id).await {
-        Some(id) => id,
-        None => {
-            check_msg(msg.channel_id.say(&ctx.http, "DMs not supported").await);
-
-            return Ok(());
-        },
-    };
-
-    let manager_lock = ctx.data.read().await
-        .get::<VoiceManager>().cloned().expect("Expected VoiceManager in TypeMap.");
-    let mut manager = manager_lock.lock().await;
-
-    if let Some(handler) = manager.join(guild_id, connect_to) {
-        handler.listen(Some(Arc::new(Receiver::new())));
-        check_msg(msg.channel_id.say(&ctx.http, &format!("Joined {}", connect_to.mention())).await);
-    } else {
-        check_msg(msg.channel_id.say(&ctx.http, "Error joining the channel").await);
-    }
-
-    Ok(())
-}
-
-async fn leave(ctx: &mut Context, msg: &Message) -> CommandResult {
-    let guild_id = match ctx.cache.guild_channel_field(msg.channel_id, |channel| channel.guild_id).await {
-        Some(id) => id,
-        None => {
-            check_msg(msg.channel_id.say(&ctx.http, "DMs not supported").await);
-
-            return Ok(());
-        },
-    };
-
-    let manager_lock = ctx.data.read().await.get::<VoiceManager>().cloned()
-        .expect("Expected VoiceManager in TypeMap.");
-    let mut manager = manager_lock.lock().await;
-    let has_handler = manager.get(guild_id).is_some();
-
-    if has_handler {
-        manager.remove(guild_id);
-
-        check_msg(msg.channel_id.say(&ctx.http,"Left voice channel").await);
-    } else {
-        check_msg(msg.reply(ctx, "Not in a voice channel").await);
-    }
-
-    Ok(())
-}
-
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    check_msg(msg.channel_id.say(&ctx.http,"Pong!").await);
-
-    Ok(())
-}*/
-
-/// Checks that a message successfully sent; if not, then logs why to stdout.
-fn check_msg(result: SerenityResult<Message>) {
-    if let Err(why) = result {
-        println!("Error sending message: {:?}", why);
-    }
-}
 
 #[serenity::async_trait]
 impl EventHandler for Handler {
     async fn message(&self, mut ctx: Context, message: Message) {
         let manager_lock = ctx.data.read().await.get::<VoiceManager>().cloned().expect("Expected VoiceManager in ShareMap.");
-        let mut manager = manager_lock.lock();
+        let manager = manager_lock.lock();
         if message.content.starts_with("r!record") {
             let voiceid = message.content.trim_start_matches("r!record ");
             print!("{}", voiceid);
@@ -191,7 +111,7 @@ impl EventHandler for Handler {
                     match guild {
                         Some(i) => {
                             if let Some(handler) = manager.await.join(i, ChannelId(x)) {
-                                handler.listen(Some(Arc::new(VoiceManager::new())));
+                                handler.listen(Some(Arc::new(Receiver)));
                                 println!("right track, wrong train");
                             }
                             else {
